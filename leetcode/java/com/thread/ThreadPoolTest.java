905f3a8c7bfc5998510ff36641cb71b7ec2248dd
rensong.pu <rensong.pu@vipshop.com>
2016-09-20 15:47:20 +0800

    newFixedThreadPool内存溢出测试，需要主动shutdown

--- leetcode/java/com/thread/ThreadPoolTest.java
+++ leetcode/java/com/thread/ThreadPoolTest.java
@@ -1,5 +1,7 @@
 package com.thread;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -7,17 +9,45 @@ import java.util.concurrent.Executors;
  * Created by rensong.pu on 2016/9/19.
  */
 public class ThreadPoolTest {
-    public static void main(String[] args) {
-        ExecutorService pool = Executors.newFixedThreadPool(3);
-        for (int i = 0; i < 100; i++) {
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println(Thread.currentThread().getName());
-                }
-            };
-            pool.submit(runnable);
-        }
+//    private static final ExecutorService pool = Executors.newFixedThreadPool(1);
 
+    public static void main(String[] args) {
+        int pid = getPid();
+        System.out.println(pid);
+        int i = 0;
+        for (int j = 0; j < 100; j++) {
+            new ThreadPoolTest().runPool();
+        }
     }
+
+    public void runPool() {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        if (pool != null) {
+            pool.submit(new Runnable() {
+                            public void run() {
+                                System.out.println(Thread.currentThread().getName());
+                                try {
+                                    System.out.println(1 / 0);
+                                    Thread.sleep(1000);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                       );
+//                pool.shutdown();
+        }
+//                pool = null;
+    }
+
+    public static int getPid() {
+        RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
+        String name = runtime.getName(); // format: "pid@hostname"
+        try {
+            return Integer.parseInt(name.substring(0, name.indexOf('@')));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
 }
